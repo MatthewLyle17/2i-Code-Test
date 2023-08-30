62160c9b2162c2a075cc9d9b1d819f19610d805a
Matthew Lyle <matthew.lyle@icloud.com>
2023-08-30 14:17:58 +0100

    added

---
+++ main.js
@@ -0,0 +1,19 @@
+let x = 4;
+let array = [2, 3, 1, 5]
+
+    for(let i = 0; i < array.length; i++){
+         if(array[i] + array[i] === x){
+            console.log("is Add");
+        } 
+        else if(array[i] - array[i] === x){
+            console.log("is Subtract");
+        } 
+        else if(array[i] * array[i] === x){
+            console.log("is Multiply" + x);
+        } 
+        else if(array[i] / array[i] === x){
+            console.log("is Divide" + x);
+        } 
+    
+}
+
